24649023508955ff3735687531e0ddd84ffdd20b
Mathieu Débit <mathieu.debit@gmail.com>
2017-03-21 21:22:59 +0100

    refactor(chat): Add classNames package

--- src/components/chat/MessageItem.js
+++ src/components/chat/MessageItem.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import classNames from 'classnames';
 import firebaseApi from '../../api/firebase';
 
 const MessageItem = (props) => {
   const currentUserEmail = firebaseApi.currentUser().email;
-  const isAuthorCurrentUser = currentUserEmail == props.message.author
-  const containerClasses = isAuthorCurrentUser ? '-current-user' : '';
+  const isAuthorCurrentUser = (currentUserEmail == props.message.author);
+
+  const containerClasses = classNames(
+    'chat-module-message-container',
+    { 'chat-module-message-container-current-user': isAuthorCurrentUser },
+  );
 
   return (
-    <div className={`chat-module-message-container${containerClasses}`}>
+    <div className={containerClasses}>
       { !isAuthorCurrentUser &&
         <div className="chat-module-message-author">{props.message.author}</div>
       }
